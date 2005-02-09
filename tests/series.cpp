b3744a1d5293819d7abf1a530967ef805b8a196f
drbob <drbob>
2005-02-09 00:12:50 +0000

    I added a sum() feature for the time series so that we can easily sum the values in a series between two dates, inclusive. This is going to be used a lot in the Factory++.

---
+++ tests/series.cpp
@@ -0,0 +1,24 @@
+/*
+ * This is a test program that exercises the CKTimeSeries.
+ */
+
+#include <iostream>
+#include <stdio.h>
+
+#include "CKTimeSeries.h"
+
+int main(int argc, char *argv[]) {
+	CKTimeSeries	a;
+
+	a.put(20041229.000000, 1.0);
+	a.put(20041230.000000, 1.5);
+	a.put(20041231.000000, 1.0);
+	a.put(20050102.000000, 2.0);
+	a.put(20050103.000000, 2.5);
+	a.put(20050104.000000, 3.5);
+	a.put(20050105.000000, 2.5);
+	a.put(20050106.000000, 1.5);
+	std::cout << a << std::endl;
+	std::cout << "sum()=" << a.sum() << std::endl;
+	std::cout << "sum(20050101, 20050105)=" << a.sum((double)20050101, (double)20050105) << std::endl;
+}
