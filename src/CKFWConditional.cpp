736de704cd3edbf8ff9c188cf85ca1bae35a4db4
Bob Beaty <drbob@themanfromspud.com>
2006-05-19 19:34:07 +0000

    I wanted to clean up the class formatting, and I also fixed a problem in the timed lockAndTest() with the rollover of the nsec. to sec. Very nice.

--- src/CKFWConditional.cpp
+++ src/CKFWConditional.cpp
@@ -1,7 +1,7 @@
 /*
  * CKFWConditional.h - this file defines the conditional waiter.
  *
- * $Id: CKFWConditional.cpp,v 1.9 2004/09/20 16:19:30 drbob Exp $
+ * $Id: CKFWConditional.cpp,v 1.10 2006/05/19 19:34:07 drbob Exp $
  */
 
 //	System Headers
@@ -52,9 +52,9 @@ CKFWConditionalDefaultTest::~CKFWConditionalDefaultTest()
 {
 }
 
-int CKFWConditionalDefaultTest::test( )
+int CKFWConditionalDefaultTest::test()
 {
-  return 1;
+	return 1;
 }
 
 
@@ -64,38 +64,44 @@ int CKFWConditionalDefaultTest::test( )
 //
 
 CKFWConditional::CKFWConditional( CKFWMutex & aMutex ) : 
-  mMutex( aMutex ),
-  mConditional( )
+	mMutex(aMutex),
+	mConditional()
 {  
-  int lResults = 0;   
-  if ( ( lResults = pthread_cond_init( &mConditional, 0 ) ) != 0 ) {
-    throw CKErrNoException( __FILE__, __LINE__, lResults );
-  }
-    
-  return ;
+	int lResults = 0;   
+	if ((lResults = pthread_cond_init(&mConditional, 0)) != 0) {
+		throw CKErrNoException(__FILE__, __LINE__, lResults);
+	}
 }
 
-CKFWConditional::~CKFWConditional( )
+CKFWConditional::~CKFWConditional()
 {
-  pthread_cond_destroy( &mConditional );
+  pthread_cond_destroy(&mConditional);
 }
 
 
 int CKFWConditional::lockAndTest( ICKFWConditionalSpuriousTest & aTest,
-				int aTimeoutInMillis )
+								  int aTimeoutInMillis )
 {
 	int lResult = FWCOND_LOCK_SUCCESS;
-  mMutex.lock( );
+	mMutex.lock();
 
-  while( aTest.test() ) {
-		if ( aTimeoutInMillis >= 0 ) {
-			timespec lTimeSpec; 
-			timeval lCurrentTimeval;
+	while(aTest.test()) {
+		if (aTimeoutInMillis >= 0) {
+			// get the current time of day in sec and nsec.
+			timeval		lCurrentTimeval;
 			gettimeofday(&lCurrentTimeval, NULL);
-			lTimeSpec.tv_sec = lCurrentTimeval.tv_sec + aTimeoutInMillis / 1000; 
-			lTimeSpec.tv_nsec = lCurrentTimeval.tv_usec * 1000
-				+ (aTimeoutInMillis % 1000) * 1000000;
+			// now populate when the timeout will occur based on the duration
+			timespec	lTimeSpec; 
+			lTimeSpec.tv_sec = lCurrentTimeval.tv_sec + aTimeoutInMillis/1000; 
+			lTimeSpec.tv_nsec = ((aTimeoutInMillis % 1000)*1000
+								 + lCurrentTimeval.tv_usec) * 1000;
+			// if we crossed the second boundary correctly update the values
+			if (lTimeSpec.tv_nsec > 1000000000) {
+				lTimeSpec.tv_sec++;
+				lTimeSpec.tv_nsec -= 1000000000;
+			}
 
+			// now wait just that long and no longer			
 			int rc = pthread_cond_timedwait(&mConditional, &mMutex.mMutex, &lTimeSpec);
 			if (rc == ETIMEDOUT) { 
 				mMutex.unlock();
@@ -103,38 +109,37 @@ int CKFWConditional::lockAndTest( ICKFWConditionalSpuriousTest & aTest,
 				break;
 			}
 		} else {
-			pthread_cond_wait( &mConditional, &mMutex.mMutex );
+			pthread_cond_wait(&mConditional, &mMutex.mMutex);
 		}
 	}
 
 	return lResult;
 }
 
-void CKFWConditional::lockAndWait( )
+void CKFWConditional::lockAndWait()
 {
-  mMutex.lock( );
-  pthread_cond_wait( &mConditional, &mMutex.mMutex );
+	mMutex.lock();
+	pthread_cond_wait(&mConditional, &mMutex.mMutex);
 }
 
-void CKFWConditional::wakeWaiter( )
+void CKFWConditional::wakeWaiter()
 {
-  int lError;
-  if ( (lError = pthread_cond_signal( &mConditional )) ) {
-    throw CKErrNoException( __FILE__, __LINE__, lError );
-  }  
+	int lError;
+	if ((lError = pthread_cond_signal(&mConditional))) {
+		throw CKErrNoException(__FILE__, __LINE__, lError);
+	}  
 }
 
-void CKFWConditional::wakeWaiters( )
+void CKFWConditional::wakeWaiters()
 {
-  int lError;
-  if ( (lError = pthread_cond_broadcast( &mConditional )) ) {
-    throw CKErrNoException( __FILE__, __LINE__, lError );
-  }
+	int lError;
+	if ((lError = pthread_cond_broadcast(&mConditional))) {
+		throw CKErrNoException(__FILE__, __LINE__, lError);
+	}
 }
 
 void CKFWConditional::unlock()
 {
 	mMutex.unlock();
 }
-
-// vim: set ts=2:
+// vim: set ts=2 sw=2:
