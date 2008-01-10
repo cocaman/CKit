b1ec6c98569f1e6f5a0a841c5c84fd1bc38154ae
drbob <drbob>
2008-01-10 13:28:00 +0000

    I added in the test for the hash_map<>. It works, but it's awfully clunky. I'm not a big fan of the soltuion that they came up with for the hash method.

--- tests/stringTest.cpp
+++ tests/stringTest.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string>
 #include <map>
+#include <ext/hash_map>
 
 #include "CKString.h"
 
@@ -13,14 +14,14 @@ int main(int argc, char *argv[]) {
 	CKString	line = "When I went walking one day ";
 
 	std::cout << line << std::endl;
-	
+
 	line.append("I saw a very large fish. It was so big that it needed another "
 				"pond to live in. I tried to take it to a pond I knew of, but "
 				"it was too heavy for me.");
 	line += 4;
 
 	std::cout << line << std::endl;
-	
+
 	line = std::string("hi");
 	line += " I can do this";
 	line.prepend("Yo! ");
@@ -29,7 +30,7 @@ int main(int argc, char *argv[]) {
 	std::cout << "size=" << line.size() << "  length=" << line.length() <<
 		"  capacity=" << line.capacity() << std::endl;
 	std::cout << "line[2]=" << line[2] << std::endl;
-	
+
 	CKString	s;
 	s = "** " + line + " **";
 	std::cout << s << std::endl;
@@ -49,7 +50,7 @@ int main(int argc, char *argv[]) {
 	std::cout << line.mid(3,5) << std::endl;
 	std::cout << line.replace('O','*') << " " << line << std::endl;
 	std::cout << line.findLast("TU") << std::endl;
-	
+
 	std::map<CKString, CKString>	lookup;
 	lookup["one"] = "ONE";
 	lookup["two"] = "TWO";
@@ -59,7 +60,7 @@ int main(int argc, char *argv[]) {
 	for (lu = lookup.begin(); lu != lookup.end(); ++lu) {
 		std::cout << lu->first << "=>" << lu->second << std::endl;
 	}
-	
+
 	CKString	one("one");
 	CKString	one_two("one_two");
 	if ((one == one_two) || (one_two == one)) {
@@ -77,4 +78,23 @@ int main(int argc, char *argv[]) {
 	} else {
 		std::cout << one_two << " !< " << one << std::endl;
 	}
+	
+	__gnu_cxx::hash_map<CKString, int, __gnu_cxx::hash<const CKString&> > months;
+	months["january"] = 31;
+	months["february"] = 28;
+	months["march"] = 31;
+	months["april"] = 30;
+	months["may"] = 31;
+	months["june"] = 30;
+	months["july"] = 31;
+	months["august"] = 31;
+	months["september"] = 30;
+	months["october"] = 31;
+	months["november"] = 30;
+	months["december"] = 31;
+  
+	std::cout << "september -> " << months["september"] << std::endl;
+	std::cout << "april     -> " << months["april"] << std::endl;
+	std::cout << "june      -> " << months["june"] << std::endl;
+	std::cout << "november  -> " << months["november"] << std::endl;
 }
